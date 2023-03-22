df35aceddf6bf920afe2d108b7d85cfa5e2b7d6f
HTK55 <s1834153@ac.ed.uk>
2023-03-22 02:15:28 +0000

    Question 3

--- example_dev.sh
+++ example_dev.sh
@@ -16,7 +16,7 @@ RESULTS_ROOT="${ROOT}/results"
 mkdir -p ${RESULTS_ROOT}
 
 ### NAME YOUR EXPERIMENT HERE ##
-EXP_NAME="baseline"
+EXP_NAME="expand_layers"
 ################################
 
 ## Local variables for current experiment
@@ -30,6 +30,9 @@ mkdir -p ${EXP_ROOT}
 python train.py --save-dir "${EXP_ROOT}" \
                 --log-file "${EXP_ROOT}/log.out"  \
                 --data "${DATA_DIR}" \
+                --encoder-num-layers 2 \
+                --decoder-num-layers 3 \
+
                 ### ADDITIONAL ARGUMENTS HERE ###
 
 ## Prediction step
